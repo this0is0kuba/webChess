5dbfea75e4eff57239b62786d0dc61c39135839b
this0is0kuba <jakub.rakus06@gmail.com>
2023-09-12 12:48:12 +0200

    Initial commit

---
+++ src/main/java/pl/edu/agh/webChess/WebChessApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.webChess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebChessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/agh/webChess/WebChessApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.webChess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
